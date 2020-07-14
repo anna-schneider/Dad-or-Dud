619c7e7136c9cddfa85d1288963516e0a76190d7
Anna Schneider <anna.schneiderm@gmail.com>
2020-07-13 22:57:38 -0400

    Created basic CSS + API

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
+let jokes //Empty Global variable
 const getJoke = async () => {
   const url = 'https://icanhazdadjoke.com/'
   try {
-    const response = await axios.get(url)
-    const joke = Object.keys(response.data.message)
-    console.log(joke)
+    //Create empty array
+    const arrOfRequests = [];
 
-    // const select = document.querySelector()
-    // optionValues(joke, select)
+    //Iterate for 10 jokes through Axios
+    for (let i = 0; i < 10; i++) {
+      arrOfRequests[i] = axios.get(url, {
+        headers: { "Accept": "application/json" }
+      });
+    }
+
+    //Wait until all promises are fulfilled, then the responses are pushed into an empty array; Iterate through the array of 10 and populate arrayOfJokes. displayJokes takes the arrayOfJokes as parameter. 
+    return await Promise.all(arrOfRequests)
 
   } catch (error) {
     console.log(`Error: ${error}`)
   }
 }
 
-getJoke()
+getJoke().then((responses) => {
+  const arrayOfJokes = []
+  for (let i = 0; i < responses.length; i++) {
+    arrayOfJokes[i] = responses[i].data.joke;
+  }
+  displayJokes(arrayOfJokes);
+});
+
+//Array of jokes
+function displayJokes(arrayOfJokes) {
+  console.log(arrayOfJokes)
+  jokes = arrayOfJokes //Store array in "jokes" variable
+}
+
+
